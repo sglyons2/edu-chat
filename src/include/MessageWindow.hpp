3581455d7e4553bbad6421d5ea711078957933c5
Steven Lyons <sglyons2@gmail.com>
2017-09-22 18:08:37 -0400

    Better message printing in the window.

--- src/include/MessageWindow.hpp
+++ src/include/MessageWindow.hpp
@@ -2,12 +2,14 @@
 #define MESSAGEWINDOW_HPP
 
 #include <string>
+#include <vector>
 #include "Window.hpp"
 
 class MessageWindow {
 private:
 	Window *window;
-	std::string text;
+	std::vector<std::string> text;
+	void formatMessage(std::vector<std::string>& lines, std::string line);
 public:
 	MessageWindow(Window *parent);
 	~MessageWindow();
