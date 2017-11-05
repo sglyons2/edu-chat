ff74b2bc5608bac703d8681bd1f9ee873f34d402
Steven Lyons <sglyons2@gmail.com>
2017-11-05 17:47:54 -0500

    Changes to reflect to unique_ptr as well as logic in processing the command.

--- src/c++/include/IrcHandler.hpp
+++ src/c++/include/IrcHandler.hpp
@@ -1,22 +1,25 @@
 #ifndef EDUCHAT_IRCHANDLER_HPP
 #define EDUCHAT_IRCHANDLER_HPP
 
-#include <deque>
-#include <memory>
-#include <string>
-#include <type_traits>
+// namespace std
+#include <deque>        // deque
+#include <memory>       // unique_ptr<>
+#include <string>       // string
+#include <type_traits>  // is_base_of<,>
+#include <vector>       // vector
 
-#include "Handler.hpp"
-#include "Socket.hpp"
+// namespace educhat
+#include "Handler.hpp"  // Handler, message, split
+#include "Socket.hpp"   // Socket
 
 namespace educhat {
 
 	template <typename T>
 	class IrcHandler : public Handler {
-	static_assert(std::is_base_of<Socket, T>::value, "T must derive from Socket");
+	static_assert(std::is_base_of<Socket, T>::value);
 	public:
 		IrcHandler() {};
-		~IrcHandler() { };
+		~IrcHandler() {};
 
 		void handleCommand(const message &command)
 		{
@@ -28,36 +31,39 @@ namespace educhat {
 			}
 	
 			std::string first = words[0];
-			std::transform(first.begin(), first.end(), first.begin(), ::tolower);
+			std::transform(first.begin(), first.end(),
+			               first.begin(), ::tolower);
 	
-			if (first == "/connect" || first == "/c") {
-				if (words.size() > 2) {
-					server = words[1];
-					port = words[2];
-					socket.connect(server, port);
-				}
-			} else if (first == "/join" || first == "/j") {
-				if (words.size() > 1) {
-					channel = words[1];
-					socket.send("JOIN " + channel + "\r\n");
-				}
-			} else if (first == "/nick" || first == "/n") {
-				if (words.size() > 1) {
-					nick = words[1];
-					socket.send("NICK " + nick + "\r\n");
-				}
-			} else if (first == "/user" || first == "/u") {
-				if (words.size() > 4) {
-					user = words[1];
-					std::string irc_user_command = "USER " + user + " " +
-					                               words[2] + " " +
-							     	       words[3] + " " +
-							     	       words[4] + " :";
-					for (std::size_t i = 5; i < words.size()-1; ++i) {
-							irc_user_command += words[i] + " ";
+			if (first[0] == '/') {
+				if (first == "/connect" || first == "/c") {
+					if (words.size() > 2) {
+						server = words[1];
+						port = words[2];
+						socket.connect(server, port);
 					}
-					irc_user_command += words[words.size()-1] + "\r\n";
-					socket.send(irc_user_command);
+				} else if (first == "/join" || first == "/j") {
+					if (words.size() > 1) {
+						channel = words[1];
+						socket.send("JOIN " + channel + "\r\n");
+					}
+				} else if (first == "/nick" || first == "/n") {
+					if (words.size() > 1) {
+						nick = words[1];
+						socket.send("NICK " + nick + "\r\n");
+					}
+				} else if (first == "/user" || first == "/u") {
+					if (words.size() > 4) {
+						user = words[1];
+						std::string irc_user_command = "USER " + user + " " +
+						                               words[2] + " " +
+						                               words[3] + " " +
+						                               words[4] + " :";
+						for (std::size_t i = 5; i < words.size()-1; ++i) {
+								irc_user_command += words[i] + " ";
+						}
+						irc_user_command += words[words.size()-1] + "\r\n";
+						socket.send(irc_user_command);
+					}
 				}
 			} else {
 				socket.send("PRIVMSG " + command.owner + " :" + command.text + "\r\n");
@@ -66,7 +72,7 @@ namespace educhat {
 
 		bool isConnected() const { return socket.isConnected(); };
 
-		std::shared_ptr<message> getUpdate()
+		std::unique_ptr<message> getUpdate()
 		{
 			if (!isConnected()) {
 				// return connection error message?
@@ -90,7 +96,7 @@ namespace educhat {
 					if (first == "PING") {
 						socket.send("PONG\r\n");
 					} else {
-						std::shared_ptr<message> res(new message);
+						message *res = new message();
 						res->type = log_msg;
 						res->text = line;
 						res->owner = "channel";
@@ -101,7 +107,7 @@ namespace educhat {
 			}
 	
 			// take a message from messages, send it back
-			std::shared_ptr<message> msg = nullptr;
+			message *msg = nullptr;
 			if (!messages.empty()) {
 				// handle message from socket
 				// refer to RFC 2812
@@ -111,7 +117,7 @@ namespace educhat {
 			}
 	
 			// return struct message pointer
-			return msg;
+			return std::unique_ptr<message>(msg);
 		};
 
 	private:
@@ -122,7 +128,7 @@ namespace educhat {
 		std::string channel;
 		std::string user;
 		std::string nick;
-		std::deque<std::shared_ptr<message>> messages;
+		std::deque<message*> messages;
 	};
 
 } // namespace educhat
